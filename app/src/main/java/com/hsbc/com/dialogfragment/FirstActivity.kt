3b7876090a784fd43943cacd28f19a09befcfc61
deepak <deepak.borade@gmail.com>
2023-05-21 15:43:31 +0530

    1.Added a new activity 2.show toast message

--- app/src/main/java/com/hsbc/com/dialogfragment/FirstActivity.kt
+++ app/src/main/java/com/hsbc/com/dialogfragment/FirstActivity.kt
@@ -2,11 +2,13 @@ package com.hsbc.com.dialogfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class FirstActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+        Toast.makeText(this@FirstActivity,"Hello World",Toast.LENGTH_LONG).show()
     }
 }
